9333037773acff3fa7465aaa9b1cf27b569a0729
Aleksey Devyatkin <nine-4lex@mail.ru>
2024-10-07 14:28:38 +0300

    [main] Fixed master tasks discovery

--- app/celery/__init__.py
+++ app/celery/__init__.py
@@ -12,7 +12,7 @@ settings = CelerySettings()  # type: ignore[call-arg]
 app = Celery(backend=settings.backend, broker=settings.broker)
 # Don't forget to register your new tasks
 #   Modules with name `tasks.py` are detected
-app.autodiscover_tasks(["app.worker.prediction"])
+app.autodiscover_tasks(["app.celery.prediction"])
 
 
 def get_async_result(task_id: str) -> AsyncResult:
