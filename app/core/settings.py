5cdae46be0636f3e8abf2f88245c108539be2ca9
Aleksey Devyatkin <aleksey.devyatkin@waveaccess.global>
2024-10-10 17:19:16 +0300

    Updated envs

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,15 +3,15 @@ from pydantic_settings import BaseSettings
 
 
 class ServerSettings(BaseSettings):
-    root_path: str = Field("/api", alias="ML_API_SERVER_API_ROOT")
+    root_path: str = Field("/api", alias="API_ROOT")
 
 
 class CelerySettings(BaseSettings):
     backend: str = Field(
         "redis://redis:6379/0",
-        alias="ML_API_SERVER_CELERY_BROKER_URL",
+        alias="CELERY_BROKER_URL",
     )
     broker: str = Field(
         "redis://redis:6379/0",
-        alias="ML_API_SERVER_CELERY_RESULT_BACKEND",
+        alias="CELERY_RESULT_BACKEND",
     )
